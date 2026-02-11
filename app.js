28fff571f22b1916efdf4caf7fb370a88dc46dfa
okenica6768 <aronokenica@gmail.com>
2026-02-11 18:23:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,77 @@
 const products = [
+  // ===== ROCK =====
   {
     id: 1,
-    name: "Melysium Hoodie Black",
+    name: "Rocková mikina Black Edition",
     price: 79,
-    image: "img/hoodie1.jpg"
+    image: "img/hoodie1.jpg",
+    genre: "rock",
+    subType: "hoodie"
   },
   {
     id: 2,
-    name: "Melysium Hoodie Purple",
-    price: 79,
-    image: "img/hoodie2.jpg"
+    name: "Rockové tričko Vintage",
+    price: 39,
+    image: "img/tee1.jpg",
+    genre: "rock",
+    subType: "tshirt"
   },
 
-  /* ===== TEST PRODUCTS FOR SCROLL ===== */
+  // ===== RAP =====
   {
     id: 3,
-    name: "Melysium Tee White",
-    price: 39,
-    image: "img/tee1.jpg"
+    name: "Rapová mikina Purple Drop",
+    price: 79,
+    image: "img/hoodie2.jpg",
+    genre: "rap",
+    subType: "hoodie"
   },
   {
     id: 4,
-    name: "Melysium Tee Black",
-    price: 39,
-    image: "img/tee2.jpg"
+    name: "Rapová šiltovka",
+    price: 29,
+    image: "img/cap1.jpg",
+    genre: "rap",
+    subType: "cap"
   },
+
+  // ===== POP =====
   {
     id: 5,
-    name: "Melysium Cap",
-    price: 29,
-    image: "img/cap1.jpg"
+    name: "Pop tričko White Wave",
+    price: 39,
+    image: "img/tee2.jpg",
+    genre: "pop",
+    subType: "tshirt"
   },
   {
     id: 6,
-    name: "Melysium Tote Bag",
+    name: "Pop taška Limited",
     price: 25,
-    image: "img/bag1.jpg"
+    image: "img/bag1.jpg",
+    genre: "pop",
+    subType: "bag"
   },
+
+  // ===== METAL =====
   {
     id: 7,
-    name: "Melysium Longsleeve",
+    name: "Metalový longsleeve Dark",
     price: 49,
-    image: "img/long1.jpg"
+    image: "img/long1.jpg",
+    genre: "metal",
+    subType: "longsleeve"
   },
   {
     id: 8,
-    name: "Melysium Limited Hoodie",
+    name: "Metalová mikina Limited",
     price: 99,
-    image: "img/hoodie3.jpg"
+    image: "img/hoodie3.jpg",
+    genre: "metal",
+    subType: "hoodie"
   }
 ];
+
 
 const productDiv = document.getElementById("products");
 
